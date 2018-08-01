e3944ea48626d9de41624d59b4cf3f8471ca75a5
Cristina Lugo <cmlugoce@syr.edu>
2018-08-01 15:40:15 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ def new
 @user = User.new
 end
 
-def create 
+def create
   @user = User.new(user_params)
   if params[:user][:password] == params[:user][:password_confirmation] && @user.save
       session[:user_id] = @user.id
@@ -12,5 +12,5 @@ def create
       redirect_to '/users/new'
     end
   end
-  
+
 end
