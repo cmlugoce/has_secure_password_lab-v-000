f9695f5ba5864e2b50f59ae7e285d40b1393bff2
Cristina Lugo <cmlugoce@syr.edu>
2018-08-01 15:33:42 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,11 @@
 class SessionsController < ApplicationController
 
+  def create 
+    @user = User.find_by(name: params[:user][:name])
+    return head(:forbidden) unless authenticated
+    @user = user
+    session[:user_id] = @user.id
+  end
+    
+
 end 
